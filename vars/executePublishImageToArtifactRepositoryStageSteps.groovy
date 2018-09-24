cc1c55eace6dd73cf4410a4ae33ce43187038a43
Uma Palepu <umap@crosslaketech.com>
2018-09-23 18:38:37 -0700

    Added the docker port and host name.

--- vars/executePublishImageToArtifactRepositoryStageSteps.groovy
+++ vars/executePublishImageToArtifactRepositoryStageSteps.groovy
@@ -13,13 +13,15 @@ def call() {
     def dockerRegistryTag = "${dockerHostAndDockerPort}/${dockerImageTag}"
     def dockerRegistryTagLatest = "${dockerHostAndDockerPort}/${dockerImageTagLatest}"
     def internalNexusHostAndPort = "${INTERNAL_NEXUS_HOSTNAME}:${NEXUS_SERVICE_PORT_DOCKER}"
+    def dockerNexusHostAndPort = "${DOCKER_NEXUS_HOSTNAME}:${NEXUS_SERVICE_PORT_DOCKER}"
 
     sh("sudo docker build -t ${dockerImageTag} -t ${dockerImageTagLatest} -t ${dockerRegistryTag} -t ${dockerRegistryTagLatest} --build-arg JAR_FILE=${jarFile} .")
     sh("sleep 3m")
 
     withCredentials([usernamePassword(credentialsId: 'nexus', usernameVariable: 'LOGIN_USERNAME', passwordVariable: 'LOGIN_PASSWORD')]) {
-        sh("echo ${LOGIN_PASSWORD} | sudo docker login --username ${LOGIN_USERNAME} --password-stdin ${dockerHostAndDockerPort}")
+//        sh("echo ${LOGIN_PASSWORD} | sudo docker login --username ${LOGIN_USERNAME} --password-stdin ${dockerHostAndDockerPort}")
 //        sh("echo ${LOGIN_PASSWORD} | sudo docker login --username ${LOGIN_USERNAME} --password-stdin ${internalNexusHostAndPort}")
+        sh("echo ${LOGIN_PASSWORD} | sudo docker login --username ${LOGIN_USERNAME} --password-stdin ${dockerNexusHostAndPort}")
     }
 
     sh("sudo docker push ${dockerRegistryTag}")
