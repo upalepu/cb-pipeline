5c87c65bef161b8d77bbe82fbca5bfcdca0fc8df
Uma Palepu <umap@crosslaketech.com>
2018-10-01 08:05:56 -0700

    Renamed kubernetes to cbk8s to correspond to the file name change from kubernetes to cbk8s

--- vars/executeDeployToPreProductionStageSteps.groovy
+++ vars/executeDeployToPreProductionStageSteps.groovy
@@ -4,9 +4,9 @@ def call() {
     echo("Executing [Deploy to Pre-Production] stage steps...")
 
     def namespace = 'pre-production'
-    kubernetes.setupkubecfg()
-    kubernetes.apply(namespace, '8611') //need to use a different port b/c this demo is running on the same cluster and node
-    kubernetes.testContainer(namespace)
+    cbk8s.setupkubecfg()
+    cbk8s.apply(namespace, '8611') //need to use a different port b/c this demo is running on the same cluster and node
+    cbk8s.testContainer(namespace)
 
     echo("Completed [Deploy to Pre-Production] stage steps.")
 }
