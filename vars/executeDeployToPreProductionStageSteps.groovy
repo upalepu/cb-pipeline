03bc3e5b5d9dd3e75f11ca35bf45245d33ee6fec
Uma Palepu <umap@crosslaketech.com>
2018-10-07 01:20:04 -0700

    Removed cbk8s.setupkubecfg

--- vars/executeDeployToPreProductionStageSteps.groovy
+++ vars/executeDeployToPreProductionStageSteps.groovy
@@ -4,7 +4,6 @@ def call() {
     echo("Executing [Deploy to Pre-Production] stage steps...")
 
     def namespace = 'pre-production'
-//    cbk8s.setupkubecfg()
     cbk8s.apply(namespace, '8611') //need to use a different port b/c this demo is running on the same cluster and node
     cbk8s.testContainer(namespace)
 
