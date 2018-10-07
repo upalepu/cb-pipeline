51636c14a44f30100d7da848b10a9001ef16e6cf
Uma Palepu <umap@crosslaketech.com>
2018-10-06 17:06:17 -0700

    Commented out call to setup kubecfg. It's being called before the Pre-Prod stage call.

--- vars/executeDeployToProductionStageSteps.groovy
+++ vars/executeDeployToProductionStageSteps.groovy
@@ -4,7 +4,7 @@ def call() {
     echo("Executing [Deploy to Production] stage steps...")
 
     def namespace = 'production'
-    cbk8s.setupkubecfg()
+//    cbk8s.setupkubecfg()
     cbk8s.apply(namespace, '8612') //need to use a different port b/c this demo is running on the same cluster and node
     cbk8s.testContainer(namespace)
 
