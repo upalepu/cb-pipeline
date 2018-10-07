f34e541f5f3d4e6b6acfc1ecce1fbaa58feeb609
Uma Palepu <umap@crosslaketech.com>
2018-10-07 15:11:14 -0700

    Changed nexus to nexus-svc

--- vars/executePublishArtifactToArtifactRepositoryStageSteps.groovy
+++ vars/executePublishArtifactToArtifactRepositoryStageSteps.groovy
@@ -4,7 +4,7 @@ def call() {
     echo("Executing [Publish Artifact] stage steps...")
 
     if ('false' == SKIP) {
-        sh("mvn -Djacoco.skip=true -DskipSourceCompile=true -DskipTestCompile=true -Dskip.surefire.tests=true -Dskip.failsafe.tests=true -Dartifact.repo.host=nexus.management.svc.cluster.local -Dartifact.repo.port=${NEXUS_SERVICE_PORT} -s dynamic-settings.xml -e deploy")
+        sh("mvn -Djacoco.skip=true -DskipSourceCompile=true -DskipTestCompile=true -Dskip.surefire.tests=true -Dskip.failsafe.tests=true -Dartifact.repo.host=nexus-svc.management.svc.cluster.local -Dartifact.repo.port=${NEXUS_SERVICE_PORT} -s dynamic-settings.xml -e deploy")
     }
 
     echo("Completed [Publish Artifact] stage steps.")
